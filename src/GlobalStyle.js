95dd28a88d6732be223e1d65e2f2fd17d0e9b663
Kuba Tuta <tuta.jakub@gmail.com>
2023-03-13 13:44:02 +0100

    initial configuration, fonts, global styles, comps

---
+++ src/GlobalStyle.js
@@ -0,0 +1,12 @@
+const { createGlobalStyle } = require("styled-components");
+
+export const GlobalStyle = createGlobalStyle`
+  #root {
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif; 
+  }
+
+  *, ::after, ::before {
+    box-sizing: inherit;
+  }
+`
