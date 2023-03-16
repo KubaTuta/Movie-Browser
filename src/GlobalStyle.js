91adf6acbbefbf27c0cc91aaec24106d4e91bc54
Mateusszzp <mateusz.pasko.r@gmail.com>
2023-03-16 20:52:54 +0100

    added styled-components

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -3,10 +3,10 @@ const { createGlobalStyle } = require("styled-components");
 export const GlobalStyle = createGlobalStyle`
   #root {
     box-sizing: border-box;
-    font-family: 'Poppins', sans-serif; 
+    font-family: 'Poppins', sans-serif;    
   }
 
   *, ::after, ::before {
     box-sizing: inherit;
   }
-`
+`;
---
+++ src/Headre/movis/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+background: #18181B; // uwaga dodać to thema
+height:94px;
+color: white;
+`
