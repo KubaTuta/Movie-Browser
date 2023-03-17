a49a1dbfa41a119eb72d2f74bf05723e7303b0ee
Hanna Nassalska <hanna.nassalska@gmail.com>
2023-03-17 22:02:48 +0100

    Tile moved to another component

--- src/Features/Movies/index.js
+++ src/Features/Movies/index.js
@@ -1,19 +1,7 @@
 import { Container } from "../../styled";
-import { GridWrapper, Header } from "./styled";
+import { Header } from "./styled";
 import Pagination from "./Pagination";
-import {
-  Tile,
-  Poster,
-  Title,
-  Year,
-  GenersWrapper,
-  Genre,
-  Star,
-  VoteWrapper,
-  VoteAverange,
-  VoteCount,
-  GenerWrapper
-} from "./Tile/styled";
+import Tile from "./Tile";
 const exampleData = require("./examplePopularMovies.json");
 
 const Movies = () => {
@@ -22,25 +10,7 @@ const Movies = () => {
       <Header>
         Popular movies
       </Header>
-      <GridWrapper>
-        {exampleData.results.map(({ original_title, id, release_date, vote_average, vote_count }) => (
-          <Tile key={id}>
-            <Poster />
-            <Title>{original_title}</Title>
-            <Year>{release_date}</Year>
-            <GenersWrapper>
-              <GenerWrapper><Genre>Action</Genre></GenerWrapper>
-              <GenerWrapper><Genre>Comedy</Genre></GenerWrapper>
-              <GenerWrapper><Genre>Drama</Genre></GenerWrapper>
-            </GenersWrapper>
-            <VoteWrapper>
-              <Star />
-              <VoteAverange> {vote_average}</VoteAverange>
-              <VoteCount>{vote_count} votes</VoteCount>
-            </VoteWrapper>
-          </Tile>
-        ))}
-      </GridWrapper>
+      <Tile />
       <Pagination
         page={exampleData.page}
         total={exampleData.total_pages}
