936886b74bf671f4513da99de7abec615f731f8e
Hanna Nassalska <hanna.nassalska@gmail.com>
2023-03-21 21:42:26 +0100

    code formatted

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -1,15 +1,15 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-    margin: 0 auto;
-    max-width: 1368px;
-    @media(max-width: ${({ theme }) => theme.breakpoint.full}) {
-        max-width: 1020px;
+  margin: 0 auto;
+  max-width: 1368px;
+  @media(max-width: ${({ theme }) => theme.breakpoint.full}) {
+    max-width: 1020px;
   }
-  @media(max-width: ${({ theme }) => theme.breakpoint.medium}) {
-    max-width: 600px;
-  }
-  @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
-    width: 288px;
-  }
+    @media(max-width: ${({ theme }) => theme.breakpoint.medium}) {
+      max-width: 600px;
+    }      
+      @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
+        width: 288px;
+      }
   `;
