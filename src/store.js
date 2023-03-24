f2023d9bdf07259aee885b7e8282eadc5499f0f3
Mateusszzp <mateusz.pasko.r@gmail.com>
2023-03-24 15:52:06 +0100

    change name file in store

--- src/store.js
+++ src/store.js
@@ -2,13 +2,13 @@ import createSagaMiddleware from '@redux-saga/core';
 import { configureStore } from '@reduxjs/toolkit';
 import rootSaga from './rootSaga';
 import popularMoviesReducer from './Features/Movies/popularMoviesSlice';
-import popularPeopleReduser from './Features/People/popularPeopleSlice';
+import popularPeopleReducer from './Features/People/popularPeopleSlice';
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     popularMovies: popularMoviesReducer,
-    popularPeople: popularPeopleReduser,
+    popularPeople: popularPeopleReducer,
   },
   middleware: [sagaMiddleware]
 });
