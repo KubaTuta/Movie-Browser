93df095eb9cc927e1a89b32f1d8d4b559f656728
Kuba Tuta <tuta.jakub@gmail.com>
2023-03-26 15:26:19 +0200

    props handled to common pagination component

--- src/Features/Movies/index.js
+++ src/Features/Movies/index.js
@@ -1,16 +1,21 @@
+import { useSelector } from "react-redux";
+import { selectPages } from "./popularMoviesSlice";
+import { fetchMoviesPending } from "./popularMoviesSlice";
 import { Container } from "../../common/Container/styled";
 import { Header } from "../../common/Header/styled";
 import Pagination from "../../common/Pagination/index";
 import Tile from "./Tile";
 
 const Movies = () => {
+  const page = useSelector(selectPages);
+
   return (
     <Container>
       <Header>
         Popular movies
       </Header>
       <Tile />
-      <Pagination />
+      <Pagination page={page} fetchApi={fetchMoviesPending}/>
     </Container>
   )
 };
