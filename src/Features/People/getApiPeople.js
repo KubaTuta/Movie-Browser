ff152d0ae615a7abd33e8ae6df06b250ab2e7ee3
Kuba Tuta <tuta.jakub@gmail.com>
2023-03-26 15:27:35 +0200

    page props added

--- src/Features/People/getApiPeople.js
+++ src/Features/People/getApiPeople.js
@@ -1,9 +1,8 @@
-export const getApiPeople = async () => {
-    const response = await fetch('https://api.themoviedb.org/3/person/popular?api_key=fef5caf4726934d4be6d4893838ad041&page');
+export const getApiPeople = async (page) => {
+    const response = await fetch(`https://api.themoviedb.org/3/person/popular?api_key=fef5caf4726934d4be6d4893838ad041&page=${page}`);
     
     if(!response.ok){
       throw new Error(response.statusText);
     }
     return await response.json();
-    
   }
