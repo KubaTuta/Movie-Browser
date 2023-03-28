95291962c99e90c2a2c7a63cf65832014337403c
Kuba Tuta <tuta.jakub@gmail.com>
2023-03-28 15:33:17 +0200

    restyling @media and template-rows

--- src/Features/Movies/Core/Success/Tile/styled.js
+++ src/Features/Movies/Core/Success/Tile/styled.js
@@ -3,84 +3,100 @@ import { ReactComponent as StarIco } from "./star.svg"
 
 export const GridWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(324px, 1fr));
   grid-gap: 24px;
-
-  @media(max-width: ${({ theme }) => theme.breakpoint.full}) {
-    grid-template-columns: repeat(3, 1fr);
-  }
-    @media(max-width: ${({ theme }) => theme.breakpoint.medium}) {
-      grid-template-columns: repeat(2, 1fr);
-    }
-      @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
-        grid-template-columns: 1fr;
-      }
+  @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
+    grid-template-columns: repeat(auto-fit, minmax(288px, 1fr));
+    grid-gap: 16px;
+  } ;
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    grid-template-columns: repeat(auto-fit, minmax(288px, 1fr));
+    grid-gap: 16px;
+  } ;
 `;
 
 export const TileWrapper = styled.div`
-max-width: 324px;
-background-color: ${({ theme }) => theme.color.white};
-box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
-border-radius: 5px;
-padding: 16px;
-display: flex;
-flex-direction: column;
-@media(max-width: ${({ theme }) => theme.breakpoint.full}) {
-   max-width: 400px;
-}
-  @media(max-width: ${({ theme }) => theme.breakpoint.medium}) {
-    display: flex;
+  height: 650px;
+  width: 100%;
+  background-color: ${({ theme }) => theme.color.white};
+  box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
+  border-radius: 5px;
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+  @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
     flex-direction: row;
-    padding: 16px;
+    height: 201px;
     gap: 16px;
-  } 
+  } ;
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    flex-direction: row;
+    height: 201px;
+    gap: 16px;
+  } ;
 `;
 
 export const Poster = styled.img`
-width: 292px;
-height: 434px;
-border-radius: 5px;
-margin-bottom: 8px;
-  @media(max-width: ${({ theme }) => theme.breakpoint.medium}) {
-    width: 114px;
+  width: 100%;
+  height: 434px;
+  object-fit: cover;
+  object-position: top;
+  border-radius: 5px;
+  margin-bottom: 8px;
+  @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
     height: 169px;
-  }
+    width: 114px;
+  } ;
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    height: 169px;
+    width: 114px;
+  } ;
 `;
 
 export const TextInfoWrapper = styled.div`
-height: 100%;
-display: flex;
-flex-direction: column;
-justify-content: space-between;
-@media(max-width: ${({ theme }) => theme.breakpoint.medium}) {
-  justify-content: flex-start;
-  }
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
-export const TextWrapper = styled.div``
+export const MovieData = styled.div`
+`;
+
 
 export const Title = styled.h3`
-font-weight: 500;
-font-size: 22px;
-margin: 0px;
-@media(max-width: ${({ theme }) => theme.breakpoint.medium}) {
-  font-style: normal;
   font-weight: 500;
-  font-size: 16px;
-}
+  font-size: 22px;
+  margin: 0px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
+    font-style: normal;
+    font-size: 16px;
+    white-space: pre-wrap;
+  };
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    font-style: normal;
+    font-size: 16px;
+    white-space: pre-wrap;
+  };
 `;
 
 export const Year = styled.p`
-font-weight: 400;
-font-size: 16px;
-line-height: 150%;
-color: ${({ theme }) => theme.color.darkerGrey};
-margin: 8px 0px;
-@media(max-width: ${({ theme }) => theme.breakpoint.medium}) {
-  font-style: normal;
   font-weight: 400;
-  font-size: 13px;
-}
+  font-size: 16px;
+  line-height: 150%;
+  color: ${({ theme }) => theme.color.darkerGrey};
+  margin: 8px 0px;
+  @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
+    font-style: normal;
+    font-size: 13px;
+  };
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    font-style: normal;
+    font-size: 13px;
+  };
 `;
 
 
@@ -94,11 +110,16 @@ export const Star = styled(StarIco)`
 padding: 0px 8px 0px 0px;
 max-width: 24px;
 max-height: 24px;
-@media(max-width: ${({ theme }) => theme.breakpoint.medium}) {
+@media(max-width: ${({ theme }) => theme.breakpoint.small}) {
   max-width: 16px;
   max-height: 16px;
   padding: 0px;
-}
+};
+@media(max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+  max-width: 16px;
+  max-height: 16px;
+  padding: 0px;
+};
 `;
 
 export const VoteAverange = styled.p`
@@ -108,10 +129,14 @@ font-size: 16px;
 line-height: 150%;
 padding: 0px 8px 0px 0px;
 margin: 0px;
-@media(max-width: ${({ theme }) => theme.breakpoint.medium}) {
+@media(max-width: ${({ theme }) => theme.breakpoint.small}) {
   font-size: 13px;
   padding: 0px 8px 0px 8px;
-}
+};
+@media(max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+  font-size: 13px;
+  padding: 0px 8px 0px 8px;
+};
 `;
 
 export const VoteCount = styled.p`
@@ -121,7 +146,10 @@ font-size: 16px;
 line-height: 150%;
 color: ${({ theme }) => theme.color.grey};
 margin: 0px;
-@media(max-width: ${({ theme }) => theme.breakpoint.medium}) {
+@media(max-width: ${({ theme }) => theme.breakpoint.small}) {
+  font-size: 13px;
+}
+@media(max-width: ${({ theme }) => theme.breakpoint.mobile}) {
   font-size: 13px;
 }
 `;
