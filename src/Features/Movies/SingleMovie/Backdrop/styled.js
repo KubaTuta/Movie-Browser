2b98176175a2335c20295db3c60ab00ade6a390c
Hanna Nassalska <hanna.nassalska@gmail.com>
2023-03-30 22:48:08 +0200

    path updated

--- src/Features/Movies/SingleMovie/Backdrop/styled.js
+++ src/Features/Movies/SingleMovie/Backdrop/styled.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
-import { ReactComponent as StarIco } from "../../../../common/Ico/star.svg"
+import { ReactComponent as StarIco } from "../../../../common/image/star.svg"
 import { Header } from "../../../../common/Header/styled";
+
 export const BlackContainer = styled.div`
  background: ${({ theme }) => theme.color.woodsmoke};
  max-height: 770px; 
