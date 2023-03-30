5d1870317d97e16eaee8a01d7593d100f386fb34
Hanna Nassalska <hanna.nassalska@gmail.com>
2023-03-30 10:28:43 +0200

    GridWrapper moved to separate component - MoviesGridWrapper

---
+++ src/common/MoviesGridWrapper/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const MoviesGridWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(324px, 1fr));
+  grid-gap: 24px;
+
+  @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
+    grid-template-columns: repeat(auto-fit, minmax(288px, 1fr));
+    grid-gap: 16px;
+  };
+`;
