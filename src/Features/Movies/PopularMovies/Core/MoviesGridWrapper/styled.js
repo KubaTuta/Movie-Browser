e53ce38fde0c3cca4e09676b9d032bf6dece60c9
Hanna Nassalska <hanna.nassalska@gmail.com>
2023-03-31 23:43:28 +0200

    MovieGridWrapper update

--- src/Features/Movies/PopularMovies/Core/MoviesGridWrapper/styled.js
+++ src/Features/Movies/PopularMovies/Core/MoviesGridWrapper/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const MoviesGridWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(324px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(288px, 1fr));
   grid-gap: 24px;
 
   @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
