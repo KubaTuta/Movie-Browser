61cea2b880f69ae1badbe4618fdb2861c2b61906
Hanna Nassalska <hanna.nassalska@gmail.com>
2023-03-31 20:40:46 +0200

    Container updated

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -3,11 +3,5 @@ import styled from "styled-components";
 export const Container = styled.main`
   margin: 0 auto;
     max-width: 1368px;
-    @media(max-width: ${({ theme }) => theme.breakpoint.full}) {
-      max-width: 1020px;
-    }
-      @media(max-width: ${({ theme }) => theme.breakpoint.medium}) {
-        max-width: 592px;
-      }
-        
+  
   `;
