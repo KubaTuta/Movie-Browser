f314f8ea3596de9e34520fb91e8a4a30b5394136
Hanna Nassalska <hanna.nassalska@gmail.com>
2023-03-31 23:39:26 +0200

    grid instead of flex in TileWrapperPeople

--- src/common/PersonTileStyles/styled.js
+++ src/common/PersonTileStyles/styled.js
@@ -2,40 +2,38 @@ import styled from "styled-components";
 
 export const WrapperPeople = styled.section`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(230px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(208px, 1fr));
   grid-gap: 24px;
   justify-items: center;
-
-  
-  @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
-    grid-template-columns: repeat(auto-fit, minmax(136px, 1fr));
-    grid-gap: 16px;
+  justify-content: space-between;
+    @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
+      grid-template-columns: repeat(auto-fit, minmax(136px, 1fr));
+      grid-gap: 16px;
     }
-
 `;
 
 export const TileWrapperPeople = styled.div`
   min-height: 339px;
-  width: 100%;
   background:${({ theme }) => theme.color.white};
   box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
+  display: grid;
+  grid-template-rows: 5fr 1fr; 
   padding: 16px;
-  @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
-    min-height: 245px;
-    width: 136px;
-    padding: 8px;
-    border-radius: 5px;
-  };
+  padding-bottom: 0;
+  gap: 16px;
+    @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
+      min-height: 245px;
+      width: 136px;
+      padding: 8px;
+      border-radius: 5px;
+      grid-template-rows: 2fr auto; 
+      gap: 8px;
+    };
 `;
 
 export const PeoplePosterWrapper = styled.div`
   min-width: 177px;
-  min-height: 256px;
- 
+  min-height: 256px;  
     @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
       min-width: 120px;
       min-height: 178px;
@@ -48,15 +46,12 @@ export const PosterPeople = styled.img`
   object-fit: cover;
   object-position: top;
   border-radius: 5px;
-
-  @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
-    height: 178px;
-    };
+    @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
+      height: 178px;
+      };
   `;
 
 export const NamePeopleWrapper= styled.div`
-  display: flex;
-  justify-content: center;
   min-height: 64px;
     @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
       min-height: 42px;
@@ -68,7 +63,7 @@ export const NamePeople = styled.p`
   font-size: 22px;
   line-height: 130%;
   margin: 0px;
-  padding-top: 8px;
+  text-align: center;
     @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
       font-size: 14px;
     };   
