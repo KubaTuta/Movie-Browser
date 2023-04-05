f0d319fae40c141398781c940a43602599db6428
Kuba Tuta <tuta.jakub@gmail.com>
2023-04-05 12:31:48 +0200

    connection of new saga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -3,11 +3,13 @@ import { genresSaga } from "./common/Genres/genresSaga";
 import { popularMoviesSaga } from "./Features/Movies/PopularMovies/popularMoviesSaga";
 import { popularPeopleSaga } from "./Features/People/PopularPeople/popularPeopleSaga";
 import { movieSaga } from "./Features/Movies/SingleMovie/movieSaga";
+import { searchedMoviesSaga } from "./common/Navigation/Input/Search/searchSaga";
 export default function* rootSaga() {
   yield all([
     popularMoviesSaga(),
     popularPeopleSaga(),
     genresSaga(),
     movieSaga(),
+    searchedMoviesSaga(),
   ]);
 }  
