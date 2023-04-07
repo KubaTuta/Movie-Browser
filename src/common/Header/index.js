109a45f8bc4b0cbd1cd21c416ad48db56217217a
Kuba Tuta <tuta.jakub@gmail.com>
2023-04-07 12:18:25 +0200

    ternary for movies header

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,5 +1,7 @@
 import { useLocation } from "react-router-dom";
 import { Header } from "./styled";
+import { useSelector } from "react-redux";
+import { selectSearchedQuery, selectSearchedTotalFrazes } from "../Navigation/Input/Search/searchSlice";
 
 const HeaderTitle = () => {
 
@@ -7,9 +9,18 @@ const HeaderTitle = () => {
 
   const isMoviesPage = location.pathname === "/";
 
+  const query = useSelector(selectSearchedQuery);
+  const results = useSelector(selectSearchedTotalFrazes);
+
   return (
     <Header>
-      {isMoviesPage ? "Popular movies" : "Popular people"}
+      {
+        isMoviesPage
+          ?
+          query.length > 0 ? `Search results for "${query}" (${results})` : "Popular movies"
+          :
+          "Popular people"
+      }
     </Header>
   )
 };
