414a6c07d5456f07994c4dc6abb47eaa46920393
Kuba Tuta <tuta.jakub@gmail.com>
2023-04-07 17:03:42 +0200

    condition for query search

--- src/Features/Movies/PopularMovies/Core/index.js
+++ src/Features/Movies/PopularMovies/Core/index.js
@@ -3,15 +3,20 @@ import { selectStatus } from "../popularMoviesSlice";
 import Loading from "../../../../common/Loading"
 import Success from "./Success";
 import ErrorPage from "../../../../common/ErrorPage";
+import { selectSearchedStatus } from "../../../../common/Navigation/Input/Search/searchSlice";
 
 const Core = () => {
 
   const status = useSelector(selectStatus);
+  const queryStatus = useSelector(selectSearchedStatus);
 
   switch (status) {
     case "pending":
       return <Loading />;
     case "success":
+      if (queryStatus === "pending") {
+        return <Loading />
+      }
       return <Success />;
     default:
       return <ErrorPage />;
