eaac3d3e81ad75264bc361336d0be0a502b9897b
Kuba Tuta <tuta.jakub@gmail.com>
2023-04-07 14:33:00 +0200

    async problem solved, query state moved up

--- src/common/Navigation/Input/index.js
+++ src/common/Navigation/Input/index.js
@@ -1,18 +1,18 @@
 import { useRef } from "react";
 import { NavInput } from "./styled";
-import { useState } from "react";
 import { fetchSearchPending } from "./Search/searchSlice";
 import { useDispatch } from "react-redux";
 
-const Input = () => {
+const Input = ({query, setQuery}) => {
 
   const input = useRef(null);
-  const [query, setQuery] = useState("");
+  
   const dispatch = useDispatch();
 
   const queryHandler = (event) => {
-    setQuery(event.target.value);
-    dispatch(fetchSearchPending( { page: 1, query: query}))
+    const newQuery = event.target.value;
+    setQuery(newQuery);
+    dispatch(fetchSearchPending({ page: 1, query: newQuery }))
   };
 
   return (
