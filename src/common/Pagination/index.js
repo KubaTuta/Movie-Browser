ccf0e44362c2b431049699b63802450e0368cf22
Kuba Tuta <tuta.jakub@gmail.com>
2023-04-07 12:06:25 +0200

    second argument to dispatches, expand total logic

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Wrapper, Button, Text, InnerButtonText, RightArrow, LeftArrow, Bold } from "./styled";
 
-const Pagination = ({ page, fetchApi }) => {
+const Pagination = ({ page, fetchApi, query, total, frazes }) => {
 
   const dispatch = useDispatch();
 
@@ -12,36 +12,38 @@ const Pagination = ({ page, fetchApi }) => {
     }
   }, [page]);
 
+  console.log(total);
+
   return (
     <Wrapper>
       <Button
         page={page}
         disabled={page === 1}
-        onClick={() => dispatch(fetchApi(1))}
+        onClick={() => dispatch(fetchApi({ page: 1, query: query }))}
       >
         <LeftArrow page={page} /><LeftArrow page={page} />&nbsp;<InnerButtonText>First</InnerButtonText>
       </Button>
       <Button
         page={page}
         disabled={page === 1}
-        onClick={() => dispatch(fetchApi(page - 1))}
+        onClick={() => dispatch(fetchApi({ page: page - 1, query: query }))}
       >
         <LeftArrow page={page} />&nbsp;<InnerButtonText>Previous</InnerButtonText>
       </Button>
-      <Text>Page</Text><Bold>{page}</Bold><Text>of</Text><Bold>500</Bold>
+      <Text>Page</Text><Bold>{page}</Bold><Text>of</Text><Bold>{total > 500 ? 500 : total}</Bold>
       <Button
         page={page}
-        total={500}
-        disabled={page === 500}
-        onClick={() => dispatch(fetchApi(page + 1))}
+        total={total > 500 ? 500 : total}
+        disabled={total > 500 ? page === 500 : page === total}
+        onClick={() => dispatch(fetchApi({ page: page + 1, query: query }))}
       >
         <InnerButtonText>Next&nbsp;</InnerButtonText><RightArrow page={page} />
       </Button>
       <Button
         page={page}
-        total={500}
-        disabled={page === 500}
-        onClick={() => dispatch(fetchApi(500))}
+        total={total > 500 ? 500 : total}
+        disabled={total > 500 ? page === 500 : page === total}
+        onClick={() => dispatch(fetchApi({ page: total, query: query }))}
       >
         <InnerButtonText>Last&nbsp;</InnerButtonText><RightArrow page={page} /><RightArrow page={page} />
       </Button>
