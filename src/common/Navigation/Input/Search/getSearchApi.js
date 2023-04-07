322d85f07fbbea2f61d47d94e67cc3125808499f
Kuba Tuta <tuta.jakub@gmail.com>
2023-04-07 12:03:45 +0200

    fetching 2 arguments in api

--- src/common/Navigation/Input/Search/getSearchApi.js
+++ src/common/Navigation/Input/Search/getSearchApi.js
@@ -1,5 +1,5 @@
-export const getSearchApi = async (query) => {
-  const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=fef5caf4726934d4be6d4893838ad041&language=en-US&query=${query}&page=1&include_adult=false`);
+export const getSearchApi = async (page, query) => {
+  const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=fef5caf4726934d4be6d4893838ad041&language=en-US&query=${query}&page=${page}&include_adult=false`);
 
   if (!response.ok) {
     throw Error(response.statusText)
