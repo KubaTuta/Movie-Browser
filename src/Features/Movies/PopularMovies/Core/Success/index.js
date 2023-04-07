ddb7e8b5fa72d26d4e9990732aa4b0e79860abf0
Kuba Tuta <tuta.jakub@gmail.com>
2023-04-07 12:08:19 +0200

    ternary and other logic to switch search / popular

--- src/Features/Movies/PopularMovies/Core/Success/index.js
+++ src/Features/Movies/PopularMovies/Core/Success/index.js
@@ -1,22 +1,45 @@
 import { useSelector } from "react-redux";
 import Pagination from "../../../../../common/Pagination";
-import { fetchMoviesPending, selectPages } from "../../popularMoviesSlice";
+import {
+  fetchMoviesPending,
+  selectPages,
+  selectTotalMovies
+} from "../../popularMoviesSlice";
 import MovieTile from "../../../../../common/MovieTile/index";
 import { selectMovies } from "../../popularMoviesSlice";
 import { MoviesGridWrapper } from "../MoviesGridWrapper/styled";
-import { selectSearchedMovies } from "../../../../../common/Navigation/Input/Search/searchSlice";
+import {
+  fetchSearchPending,
+  selectSearchedMovies,
+  selectSearchedPages,
+  selectSearchedQuery,
+  selectSearchedTotalPages,
+  selectSearchedTotalFrazes,
+} from "../../../../../common/Navigation/Input/Search/searchSlice";
 
 const Success = () => {
   const page = useSelector(selectPages);
   const movies = useSelector(selectMovies);
+  const total = useSelector(selectTotalMovies);
+
+  const searchQuery = useSelector(selectSearchedQuery);
+  const searchPage = useSelector(selectSearchedPages);
   const searchedMovies = useSelector(selectSearchedMovies);
+  const searchedTotalPages = useSelector(selectSearchedTotalPages);
+  const searchedTotalFrazes = useSelector(selectSearchedTotalFrazes);
 
   return (
     <>
-    <MoviesGridWrapper>
-      <MovieTile movies={searchedMovies ? searchedMovies : movies} />
+      <MoviesGridWrapper>
+        <MovieTile movies={searchQuery.length > 0 ? searchedMovies : movies} />
       </MoviesGridWrapper>
-      <Pagination page={page} fetchApi={fetchMoviesPending} />
+      <Pagination
+        page={searchQuery.length > 0 ? searchPage : page}
+        fetchApi={searchQuery.length > 0 ? fetchSearchPending : fetchMoviesPending}
+        query={searchQuery.length > 0 ? searchQuery : null}
+        total={searchQuery.length > 0 ? searchedTotalPages : total}
+        frazes={searchQuery.length > 0 ? searchedTotalFrazes : null}
+      />
     </>
   )
 };
