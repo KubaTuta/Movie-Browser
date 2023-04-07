4dfe304a61e0d02e105adb668b91c3e8ed3c27bd
Kuba Tuta <tuta.jakub@gmail.com>
2023-04-07 14:31:42 +0200

    cleaning handler and input props

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -18,8 +18,19 @@ import Movies from "../../Features/Movies/PopularMovies";
 import People from "../../Features/People/PopularPeople"
 import SingleMovie from "../../Features/Movies/SingleMovie";
 import Input from "./Input";
+import { useDispatch } from "react-redux";
+import { fetchSearchPending } from "./Input/Search/searchSlice";
+import { useState } from "react";
 
 const Navigation = () => {
+  const [query, setQuery] = useState("");
+  const dispatch = useDispatch();
+
+  const cleaningHandler = () => {
+    dispatch(fetchSearchPending({ page: 1, query: "" }));
+    setQuery("");
+  };
+
   return (
     <>
       <NavigationBoxStyled>
@@ -29,24 +40,38 @@ const Navigation = () => {
               <Stroke />
               <NavH1>movies browser</NavH1>
               <NavLinks           >
-                <NavLi><StyledNavLink to="/">movies</StyledNavLink></NavLi>
-                <NavLi><StyledNavLink to="/people">people</StyledNavLink></NavLi>
+                <NavLi>
+                  <StyledNavLink
+                    to="/"
+                    onClick={cleaningHandler}
+                  >
+                    movies
+                  </StyledNavLink>
+                </NavLi>
+                <NavLi>
+                  <StyledNavLink
+                    to="/people"
+                    onClick={cleaningHandler}
+                  >
+                    people
+                  </StyledNavLink>
+                </NavLi>
               </NavLinks>
             </NavBoxLine>
             <NavWrapperInput>
               <NavIconInput>
                 <Elipse />
               </NavIconInput>
-              <Input />
+              <Input query={query} setQuery={setQuery} />
             </NavWrapperInput>
           </NavigationWrapper>
         </NavBoxFrame>
 
       </NavigationBoxStyled>
       <Routes>
-        <Route path="/" element={<Movies/>} />
+        <Route path="/" element={<Movies />} />
         <Route path="/people" element={<People />} />
-        <Route path="/movie/:id" element={<SingleMovie />} />
+        <Route path="/movie/:id" element={<SingleMovie cleaningHandler={cleaningHandler} />} />
 
 
         <Route path="*" element={<Navigate to="/movies" />} />
