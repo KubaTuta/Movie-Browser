f1b2823c215c4db42e822079f2414d04188d0a5c
Kuba Tuta <tuta.jakub@gmail.com>
2023-04-09 14:57:44 +0200

    logic for search display in single person comp

--- src/Features/People/Profile/index.js
+++ src/Features/People/Profile/index.js
@@ -1,19 +1,24 @@
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
-import { useDispatch, } from "react-redux";
-import { fetchCastCrewSuccess,  fetchPending } from "./profileSlice";
+import { useDispatch, useSelector, } from "react-redux";
+import { fetchCastCrewSuccess, fetchPending } from "./profileSlice";
 import { Container } from "../../../common/Container/styled";
 import Core from "./Core";
 import { fetchGenresPending } from "../../../common/Genres/genresSlice";
+import { selectSearchedPeopleQuery } from "../../../common/Navigation/PeopleInput/Search/searchPeopleSlice";
+import People from "../PopularPeople";
 
 
-const ProfilePage = () => {
+const ProfilePage = ({ cleaningHandler }) => {
   const { id } = useParams();
   const dispatch = useDispatch();
-
+  const query = useSelector(selectSearchedPeopleQuery)
+    ;
   useEffect(() => {
-    dispatch(fetchPending(id))
+    dispatch(fetchPending(id));
     dispatch(fetchCastCrewSuccess(id));
+    cleaningHandler();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dispatch, id]);
 
   useEffect(() => {
@@ -23,11 +28,13 @@ const ProfilePage = () => {
 
 
   return (
-    <>
+    query.length > 0 ?
+      <People /> :
       <Container>
         <Core />
       </Container>
-    </>
+
+
   )
 }
 export default ProfilePage;
