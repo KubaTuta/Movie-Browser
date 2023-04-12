7d2d23ec3d244935c7398ebc3e2908e081b16fdf
Hanna Nassalska <hanna.nassalska@gmail.com>
2023-04-12 14:59:45 +0200

    InfoCategory - comma, InfoText - colon added

--- src/Features/Movies/SingleMovie/Core/TileDetails/index.js
+++ src/Features/Movies/SingleMovie/Core/TileDetails/index.js
@@ -46,17 +46,17 @@ const TileDetails = (
         <Title> {title}</Title>
         <Year>{year && year.slice(0, 4)}</Year>
         <InfoWrapper>
-          <InfoCategory>Production</InfoCategory>
+          <InfoCategory>Production:</InfoCategory>
           {
             countries && countries.map((country) => (
               <InfoTextWrapper key={country.iso_3166_1}>
-                <InfoText>{country.name}</InfoText>
+                <InfoText>{country.name},</InfoText>
               </InfoTextWrapper>
             ))
           }
         </InfoWrapper>
         <InfoWrapper>
-          <InfoCategory>Release date</InfoCategory>
+          <InfoCategory>Release date:</InfoCategory>
           <InfoText>{new Date(release).toLocaleDateString()}</InfoText>
         </InfoWrapper>
         <GenresWrapper>
