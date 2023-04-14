bb54b6c30513abe8cb5614f0bec2a0d362f3ac35
Mateusszzp <mateusz.pasko.r@gmail.com>
2023-04-14 20:52:31 +0200

    added link between pages\

--- src/Features/Movies/SingleMovie/Core/Credits/index.js
+++ src/Features/Movies/SingleMovie/Core/Credits/index.js
@@ -1,11 +1,12 @@
-import { 
-  NamePeople, 
-  NamePeopleWrapper, 
-  PeoplePosterWrapper, 
-  PosterPeople 
+import {
+  NamePeople,
+  NamePeopleWrapper,
+  PeoplePosterWrapper,
+  PosterPeople
 } from "../../../../../common/PersonTileStyles/styled";
 import { CreditWrapper, Role, CreditPeopleWrapper } from "./styled";
 import noProfilePicture from "../../../../../common/PersonTileStyles/noProfilePicture.svg";
+import { StyledLink } from "../../../../../common/MovieTile/styled";
 
 const Credits = ({ credits }) => {
 
@@ -14,20 +15,22 @@ const Credits = ({ credits }) => {
   return (
     <CreditWrapper>
       {credits && credits.map((credit, id) => (
-        <CreditPeopleWrapper key={id}>
-          <PeoplePosterWrapper>
-            {credit.profile_path ?
-              (<PosterPeople src={`${personsProfilePicturePath}${credit.profile_path}`} />)
-              : (
-                <PosterPeople src={noProfilePicture} alt="" />
-              )
-            }
-          </PeoplePosterWrapper>
-          <NamePeopleWrapper>
-            <NamePeople>{credit.name}</NamePeople>
-            <Role>{(credit.character || credit.job)}</Role>
-          </NamePeopleWrapper>
-        </CreditPeopleWrapper>
+        <StyledLink to={`/profile/${credit.id}`} key={id}>
+          <CreditPeopleWrapper>
+            <PeoplePosterWrapper>
+              {credit.profile_path ?
+                (<PosterPeople src={`${personsProfilePicturePath}${credit.profile_path}`} />)
+                : (
+                  <PosterPeople src={noProfilePicture} alt="" />
+                )
+              }
+            </PeoplePosterWrapper>
+            <NamePeopleWrapper>
+              <NamePeople>{credit.name}</NamePeople>
+              <Role>{(credit.character || credit.job)}</Role>
+            </NamePeopleWrapper>
+          </CreditPeopleWrapper>
+        </StyledLink>
       ))}
     </CreditWrapper>
   )
