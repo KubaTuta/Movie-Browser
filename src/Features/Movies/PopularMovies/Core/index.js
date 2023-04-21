177d80dd594a79837f2efbb860a906d95f41b905
Kuba Tuta <tuta.jakub@gmail.com>
2023-04-21 16:27:16 +0200

    logic for NoResult display

--- src/Features/Movies/PopularMovies/Core/index.js
+++ src/Features/Movies/PopularMovies/Core/index.js
@@ -1,14 +1,17 @@
 import { useSelector } from "react-redux";
 import { selectStatus } from "../popularMoviesSlice";
-import Loading from "../../../../common/Loading"
+import Loading from "../../../../common/Search/Loading"
 import Success from "./Success";
-import ErrorPage from "../../../../common/ErrorPage";
-import { selectSearchedStatus } from "../../../../common/Navigation/Input/Search/searchSlice";
+import ErrorPage from "../../../../common/Search/ErrorPage";
+import { selectSearchedQuery, selectSearchedStatus, selectSearchedTotalFrazes } from "../../../../common/Navigation/Input/Search/searchSlice";
+import NoResult from "../../../../common/Search/NoResult";
 
 const Core = () => {
 
   const status = useSelector(selectStatus);
   const queryStatus = useSelector(selectSearchedStatus);
+  const query = useSelector(selectSearchedQuery);
+  const results = useSelector(selectSearchedTotalFrazes);
 
   switch (status) {
     case "pending":
@@ -17,7 +20,7 @@ const Core = () => {
       if (queryStatus === "pending") {
         return <Loading />
       }
-      return <Success />;
+      return (results === 0 && query.length > 0) ? <NoResult /> : <Success />
     default:
       return <ErrorPage />;
   }
--- src/Features/People/PopularPeople/Core/index.js
+++ src/Features/People/PopularPeople/Core/index.js
@@ -1,14 +1,17 @@
 import { useSelector } from "react-redux";
-import ErrorPage from "../../../../common/ErrorPage/index";
-import Loading from "../../../../common/Loading/index";
+import ErrorPage from "../../../../common/Search/ErrorPage/index";
+import Loading from "../../../../common/Search/Loading/index";
 import { selectStatus } from "../popularPeopleSlice";
 import Success from "./Success/";
-import { selectSearchedPeopleStatus } from "../../../../common/Navigation/PeopleInput/Search/searchPeopleSlice";
+import { selectSearchedPeopleQuery, selectSearchedPeopleStatus, selectSearchedPeopleTotalFrazes } from "../../../../common/Navigation/PeopleInput/Search/searchPeopleSlice";
+import NoResult from "../../../../common/Search/NoResult";
 
 const Core = () => {
 
   const status = useSelector(selectStatus);
   const queryStatus = useSelector(selectSearchedPeopleStatus);
+  const queryPeople = useSelector(selectSearchedPeopleQuery);
+  const resultsPeople = useSelector(selectSearchedPeopleTotalFrazes);
 
   switch (status) {
     case "pending":
@@ -17,7 +20,7 @@ const Core = () => {
       if (queryStatus === "pending") {
         return <Loading />
       }
-      return <Success />;
+      return (resultsPeople === 0 && queryPeople.length > 0) ? <NoResult /> : <Success />
     default:
       return <ErrorPage />
   }
