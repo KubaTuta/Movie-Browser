fbbe6a835cc4bc2cc7bba69668db888a7db3c46f
Hanna Nassalska <hanna.nassalska@gmail.com>
2023-04-26 22:55:54 +0200

    Container - padding - small size

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -4,4 +4,7 @@ export const Container = styled.main`
   margin: 0 auto;
   max-width: 1368px;
   padding: 8px;
+    @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
+      padding: 4px;
+    };
   `;
