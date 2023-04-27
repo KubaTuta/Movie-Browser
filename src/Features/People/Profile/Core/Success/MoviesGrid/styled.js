547574be430439667aee06af671af5269ac15fd0
Hanna Nassalska <hanna.nassalska@gmail.com>
2023-04-27 12:22:30 +0200

    MoviesGrid update

--- src/Features/People/Profile/Core/Success/MoviesGrid/styled.js
+++ src/Features/People/Profile/Core/Success/MoviesGrid/styled.js
@@ -3,24 +3,12 @@ import styled from "styled-components";
 export const MoviesGrid = styled.div`
   display: grid;
   margin:0;
-  grid-template-columns: repeat(auto-fill, minmax(288px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
   grid-template-rows: 1fr;
   grid-gap: 24px;
-    @media(max-width: ${({ theme }) => theme.breakpoint.moviesMedium}) {
-      grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
-    }
-    @media(max-width: ${({ theme }) => theme.breakpoint.medium}) {
-      grid-template-columns: repeat(auto-fill, minmax(450px, 1fr));
-    }
-      @media(max-width: ${({ theme }) => theme.breakpoint.creditsMediumSmaller}) {
-        grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
-      }
-        @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
-          grid-template-columns: repeat(auto-fit, minmax(288px, 1fr));
-          grid-gap: 16px;
-        };
-          @media(max-width: ${({ theme }) => theme.breakpoint.mobile}) {
-            grid-template-columns: repeat(auto-fit, minmax(288px, 1fr));
-            grid-gap: 16px;
-          };
+    @media(max-width: ${({ theme }) => theme.breakpoint.small}) {
+      grid-template-columns: repeat(auto-fit, minmax(288px, 1fr));
+      grid-gap: 16px;
+      justify-items: center;
+};
 `;
