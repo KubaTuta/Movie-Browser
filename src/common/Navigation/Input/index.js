85250537a9551d81b88c3dd8916cbac2844fbfec
Kuba Tuta <tuta.jakub@gmail.com>
2023-04-30 21:08:04 +0200

    clearing url

--- src/common/Navigation/Input/index.js
+++ src/common/Navigation/Input/index.js
@@ -1,17 +1,22 @@
 import { useRef } from "react";
 import { NavInput } from "../styled";
-import { fetchSearchPending } from "./Search/searchSlice";
-import { useDispatch } from "react-redux";
+import { fetchSearchPending, selectSearchedPages } from "./Search/searchSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 
 const Input = ({ query, setQuery }) => {
 
   const input = useRef(null);
+  const location = useLocation();
+  console.log(location.search)
+
+  const page = useSelector(selectSearchedPages)
 
   const dispatch = useDispatch();
 
   const queryHandler = (event) => {
     const newQuery = event.target.value;
-    setQuery({ search: newQuery });
+    setQuery(!!newQuery ? { search: newQuery, page: page } : "");
     dispatch(fetchSearchPending({ page: 1, query: newQuery }))
   };
 
