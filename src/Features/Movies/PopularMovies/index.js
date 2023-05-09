abf00a4268bac4e3083e1eaba2b980cdd6070c3e
Kuba Tuta <tuta.jakub@gmail.com>
2023-05-09 08:17:32 +0200

    all fetch moved to this component

--- src/Features/Movies/PopularMovies/index.js
+++ src/Features/Movies/PopularMovies/index.js
@@ -4,6 +4,8 @@ import { useDispatch } from "react-redux";
 import { fetchMoviesPending } from "./popularMoviesSlice";
 import { fetchGenresPending } from "../../../common/Genres/genresSlice";
 import Core from "./Core";
+import { useQueryParameter } from "../../../common/useParameter";
+import { fetchSearchPending } from "../../../common/Navigation/Input/Search/searchSlice";
 
 const Movies = () => {
   const dispatch = useDispatch();
@@ -17,9 +19,19 @@ const Movies = () => {
     dispatch(fetchGenresPending())
     // eslint-disable-next-line
   }, []);
+
+  const queryParam = useQueryParameter("search");
+  const queryPage = useQueryParameter("page");
+
+  useEffect(()=>{
+    if (queryParam !==null) {
+      dispatch(fetchSearchPending({page: queryPage ?? 1, query: queryParam}))
+    }
+}, [queryParam])
+
   return (
     <Container>
-      <Core />
+      <Core queryParam={queryParam} />
     </Container>
   )
 };
