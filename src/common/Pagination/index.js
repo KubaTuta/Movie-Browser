222c583cba3cd89ef4f4a81fc845202545c2c30c
Kuba Tuta <tuta.jakub@gmail.com>
2023-05-09 09:49:15 +0200

    simplify component, rid of fetching

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -1,10 +1,27 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Wrapper, Button, Text, InnerButtonText, RightArrow, LeftArrow, Bold } from "./styled";
+import {
+  Wrapper,
+  Button,
+  Text,
+  InnerButtonText,
+  RightArrow,
+  LeftArrow,
+  Bold,
+} from "./styled";
+import { useReplaceQueryParameter } from "../useParameter";
 
-const Pagination = ({ page, fetchApi, query, total }) => {
+const Pagination = ({ page, total }) => {
 
-  const dispatch = useDispatch();
+  const replaceQueryParameter = useReplaceQueryParameter();
+
+  const setPage = (destinationPage) => {
+    replaceQueryParameter([
+      {
+        key: "page",
+        value: destinationPage,
+      },
+    ]);
+  };
 
   useEffect(() => {
     if (page !== 1) {
@@ -17,36 +34,45 @@ const Pagination = ({ page, fetchApi, query, total }) => {
       <Button
         page={page}
         disabled={page === 1}
-        onClick={() => dispatch(fetchApi({ page: 1, query: query }))}
+        onClick={() => setPage(1)}
       >
-        <LeftArrow page={page} /><LeftArrow page={page} />&nbsp;<InnerButtonText>First</InnerButtonText>
+        <LeftArrow page={page} />
+        <LeftArrow page={page} />
+        &nbsp;<InnerButtonText>First</InnerButtonText>
       </Button>
       <Button
         page={page}
         disabled={page === 1}
-        onClick={() => dispatch(fetchApi({ page: page - 1, query: query }))}
+        onClick={() => setPage(page - 1)}
       >
-        <LeftArrow page={page} />&nbsp;<InnerButtonText>Previous</InnerButtonText>
+        <LeftArrow page={page} />
+        &nbsp;<InnerButtonText>Previous</InnerButtonText>
       </Button>
-      <Text>Page</Text><Bold>{page}</Bold><Text>of</Text><Bold>{total > 500 ? 500 : total}</Bold>
+      <Text>Page</Text>
+      <Bold>{page}</Bold>
+      <Text>of</Text>
+      <Bold>{total > 500 ? 500 : total}</Bold>
       <Button
         page={page}
         total={total > 500 ? 500 : total}
         disabled={total > 500 ? page === 500 : page === total}
-        onClick={() => dispatch(fetchApi({ page: page + 1, query: query }))}
+        onClick={() => setPage(page + 1)}
       >
-        <InnerButtonText>Next&nbsp;</InnerButtonText><RightArrow page={page} />
+        <InnerButtonText>Next&nbsp;</InnerButtonText>
+        <RightArrow page={page} />
       </Button>
       <Button
         page={page}
         total={total > 500 ? 500 : total}
         disabled={total > 500 ? page === 500 : page === total}
-        onClick={() => dispatch(fetchApi({ page: (total > 500 ? 500 : total), query: query }))}
+        onClick={() => setPage(total > 500 ? 500 : total)}
       >
-        <InnerButtonText>Last&nbsp;</InnerButtonText><RightArrow page={page} /><RightArrow page={page} />
+        <InnerButtonText>Last&nbsp;</InnerButtonText>
+        <RightArrow page={page} />
+        <RightArrow page={page} />
       </Button>
     </Wrapper>
-  )
+  );
 };
 
 export default Pagination;
